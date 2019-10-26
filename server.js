fbf765345e68aacc4a1ff53b39d583daa91d4b44
Scottie <avscott@pacbell.net>
2019-10-26 08:28:16 -0700

    initial heroku commit

---
+++ server.js
@@ -0,0 +1,30 @@
+// Dependencies
+// =============================================================
+require('dotenv').config();
+const express = require("express");
+const path = require("path");
+
+// Sets up the Express App
+// =============================================================
+let app = express();
+let PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+app.get("/", function(req, res) {
+    res.json(path.join(__dirname, "public/index.html"));
+});
+
+
+
+
+
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+// Log (server-side) when our server has started
+console.log("Server listening on: http://localhost:" + PORT);
+});
