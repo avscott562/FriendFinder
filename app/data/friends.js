1ec5b755debf7f7e3dd933804b51f4b22c70b08b
Scottie <avscott@pacbell.net>
2019-10-26 08:59:24 -0700

    add routes and friends array

---
+++ app/data/friends.js
@@ -0,0 +1,66 @@
+let friends = [
+    {
+        name: "tester",
+        photo: "URL of photo",
+        scores: [
+            1,
+            2,
+            5,
+            4,
+            2,
+            3,
+            5,
+            4,
+            5,
+            5
+        ]
+    },
+    {
+        name: "test 2",
+        photo: "URL of photo",
+        scores: [
+            1,
+            2,
+            5,
+            4,
+            2,
+            3,
+            5,
+            4,
+            5,
+            5
+        ]
+    },
+    {
+        name: "tester 3",
+        photo: "URL of photo",
+        scores: [
+            1,
+            2,
+            5,
+            4,
+            2,
+            3,
+            5,
+            4,
+            5,
+            5
+        ]
+    },
+    {
+        name: "tester 4",
+        photo: "URL of photo",
+        scores: [
+            1,
+            2,
+            5,
+            4,
+            2,
+            3,
+            5,
+            4,
+            5,
+            5
+        ]
+    },
+]
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,45 @@
+// Dependencies
+// =============================================================
+require('dotenv').config();
+const express = require("express");
+const path = require("path");
+
+// Sets up the Express App
+// =============================================================
+let app = express();
+let PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+});
+
+app.get("/api/friends", function(req, res) {
+    res.json(friends);
+});
+
+app.post("/api/friends", function(req, res) {
+    let newFriend = req.body;
+    newFriend.routeName = newFriend.name.replace(/\s+/g, "").toLowerCase();
+
+    console.log(newFriend);
+  
+    friends.push(newFriend);
+  
+    res.json(newFriend);
+});
+
+
+
+
+
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+// Log (server-side) when our server has started
+console.log("Server listening on: http://localhost:" + PORT);
+});
