e987b5c66515f2f215e040cfd602e427f47051f2
Scottie <avscott@pacbell.net>
2019-10-26 19:28:00 -0700

    add code to home.html

--- app/data/friends.js
+++ app/data/friends.js
@@ -19,30 +19,30 @@ let friends = [
         name: "test 2",
         photo: "URL of photo",
         scores: [
-            1,
-            2,
             5,
-            4,
+            3,
+            5,
+            5,
             2,
             3,
             5,
             4,
-            5,
-            5
+            1,
+            1
         ]
     },
     {
         name: "tester 3",
         photo: "URL of photo",
         scores: [
-            1,
-            2,
+            3,
+            4,
             5,
             4,
-            2,
+            1,
+            3,
             3,
             5,
-            4,
             5,
             5
         ]
@@ -52,15 +52,15 @@ let friends = [
         photo: "URL of photo",
         scores: [
             1,
+            1,
             2,
-            5,
             4,
-            2,
+            5,
             3,
-            5,
-            4,
-            5,
-            5
+            1,
+            1,
+            2,
+            2
         ]
     },
 ]
