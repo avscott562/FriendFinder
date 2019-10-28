286bf3718928f2d41bf828de4bcd06d2289e19c3
Scottie <avscott@pacbell.net>
2019-10-27 23:38:43 -0700

    fix post ajax requests

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,24 +1,24 @@
 //array of all of the friends
 let friends = [
     {
-        name: "tester",
+        name: "Rhonda Lester",
         photo: "https://forwardtogether.org/wp-content/uploads/2016/01/Black-Woman-Smiling.jpg",
         scores: [1, 2, 5, 4, 2, 3, 5, 4, 5, 5]
     },
     {
-        name: "test 2",
+        name: "Roscoe Jenkins",
         photo: "https://ak0.picdn.net/shutterstock/videos/2991310/thumb/3.jpg",
         scores: [5, 3, 5, 5, 2, 3, 5, 4, 1, 1]
     },
     {
-        name: "tester 3",
+        name: "Will Hunter",
         photo: "https://st2.depositphotos.com/1715570/5595/i/950/depositphotos_55955771-stock-photo-modern-young-black-man-smiling.jpg",
         scores: [3, 4, 5, 4, 1, 3, 3, 5, 5, 5]
     },
     {
-        name: "tester 4",
+        name: "Pat Riley",
         photo: "https://i.pinimg.com/originals/4d/8a/98/4d8a98cb29768876933e58148a2ba98c.jpg",
-        scores: [1, 1, 2, 4, 5, 3, 1, 1, 2, 2]
+        scores: [1, 5, 2, 4, 5, 3, 1, 1, 2, 2]
     },
 ]
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -9,21 +9,45 @@ let friends = require("../data/friends.js");
 
 //export data from server
 module.exports = function (app) {
+
     app.get("/api/friends", function(req, res) {
         res.json(friends);
     });
     
     app.post("/api/friends", function(req, res) {
-        mewFriend = req.body;
+        
+        let match = {
+            name: "",
+            photo: "",
+            difference: 100
+        };
 
-        let difference = 100;
-      
+        console.log(req.body);
+        
+        let newFriend = req.body;
+        let friendScores = newFriend.scores;
+
+        let currentDifference = 0;
+        
+        for (i=0; i<friends.length; i++) {
+
+            console.log(friends[i]);
+            currentDifference = 0;
+
+            for (s=0; s<friends[i].scores[s]; s++) {
+                currentDifference += Math.abs(parseInt(newFriend.scores[s]) - parseInt(friends[i].scores[s]));
+                
+                if (currentDifference <= match.difference) {
+                    match.name = friends[i].name;
+                    match.photo = friends[i].photo;
+                    match.difference = currentDifference;
+                }
+            }
+        }
        
+        friends.push(newFriend);
+        //   console.log("add to friends list");
       
-        res.json(friends[1]);
-    })
-    .done(function(newFriend) {
-         // friends.push(friend);
-         console.log("add to friends list");
+        res.json(match);
     });
 }
